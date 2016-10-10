bf979f9a929da635e495ea8f9cc759835317dc9a
Dennis Kleine <dennis.kleine@comlearn.de>
2016-10-10 13:37:43 +0200

    Implemented CRUD with List from the collection classes

--- app/models/Car.scala
+++ app/models/Car.scala
@@ -1,6 +1,12 @@
 package models
 
 import java.util.Date
+import javax.inject.Inject
+
+import play.api.db.slick.DatabaseConfigProvider
+import slick.driver.JdbcProfile
+
+import scala.collection.immutable.List
 
 /**
   * Created by denniskleine on 07.10.16.
@@ -13,16 +19,52 @@ import java.util.Date
   * first registration (only for used cars): date without time.
   */
 case class Car(id: Int,
-               title: String,
-               fuel: Fuel.Value,
-               price: Int,
-               newCar: Boolean,
-               mileage: Option[Int],
-               firstRegistration: Option[Date]
+               title: String //,
+//               fuel: Fuel.Value,
+//               price: Int,
+//               newCar: Boolean,
+//               mileage: Option[Int],
+//               firstRegistration: Option[Date]
                )
 
 object Fuel extends Enumeration {
   val Gasoline, Diesel = Value
 }
 
+object Car {
+  var cars: List[Car] = {
+    List(
+      Car(
+        11,
+        "Audi"
+      ),
+      Car(
+        12,
+        "BMW"
+      )
+    )
+  }
+
+  def list = {
+    cars
+  }
+
+  def find(id: Int) = {
+    cars.find(_.id == id)
+  }
+
+  def create(car: Car) = {
+    cars = cars ++ List(car)
+  }
+
+  def update(id: Int, car: Car) = {
+    cars = cars.filter(_.id != id)
+    cars = cars ++ List(car)
+  }
+
+  def delete(id: Int) = {
+    cars = cars.filter(_.id != id)
+  }
+
+}
 
