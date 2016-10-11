e8b5bc674263cbea6719e13851d6a14b01fd0fca
Dennis Kleine <dennis.kleine@comlearn.de>
2016-10-12 00:33:31 +0200

    now safe FIRST_REGISTRATION as bigint

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
-create table CAR (ID int primary key, TITLE varchar not null, FUEL varchar not null, PRICE int not null, NEWCAR boolean not null, MILEAGE int, FIRST_REGISTRATION date);
+create table "CAR" ("ID" bigserial primary key not null, "TITLE" varchar not null, "FUEL" varchar not null, "PRICE" int not null, "NEWCAR" boolean not null, "MILEAGE" int, "FIRST_REGISTRATION" bigint);
 
 # --- !Downs
 
-drop table CAR;
+drop table "CAR";
