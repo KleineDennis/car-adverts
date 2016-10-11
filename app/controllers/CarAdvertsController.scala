0595b274618b83c0b8c4b11409986ba79b4d905f
Dennis Kleine <dennis.kleine@comlearn.de>
2016-10-12 00:49:38 +0200

    refactoring CRUD RESTful

--- app/controllers/CarAdvertsController.scala
+++ app/controllers/CarAdvertsController.scala
@@ -4,8 +4,10 @@ import java.util.Date
 import javax.inject._
 
 import models.{Car, CarsDAO, Fuel}
+import play.api.Logger
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -17,42 +19,47 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class CarAdvertsController @Inject()(carsDao: CarsDAO)(implicit ec: ExecutionContext) extends Controller {
 
-//  implicit val carWrites: Writes[Car] = (
-//    (JsPath \ "id").write[Int] and
-//    (JsPath \ "title").write[String] and
-//    (JsPath \ "fuel").write[Fuel.Value] and
-//    (JsPath \ "price").write[Int] and
-//    (JsPath \ "newCar").write[Boolean] and
-//    (JsPath \ "mileage").write[Option[Int]] and
-//    (JsPath \ "firstRegistration").write[Option[Date]]
-//    )(unlift(Car.unapply))
-//
-//  implicit val carReads: Reads[Car] = (
-//    (JsPath \ "id").read[Int] and
-//    (JsPath \ "title").read[String] and
-//    (JsPath \ "fuel").read[Fuel.Value] and
-//    (JsPath \ "price").read[Int] and
-//    (JsPath \ "newCar").read[Boolean] and
-//    (JsPath \ "mileage").read[Option[Int]] and
-//    (JsPath \ "firstRegistration").read[Option[Date]]
-//  )(Car.apply _)
+  implicit val carWrites: Writes[Car] = (
+    (JsPath \ "id").write[Int] and
+    (JsPath \ "title").write[String] and
+    (JsPath \ "fuel").write[Fuel.Value] and
+    (JsPath \ "price").write[Int] and
+    (JsPath \ "newCar").write[Boolean] and
+    (JsPath \ "mileage").writeNullable[Int] and
+    (JsPath \ "firstRegistration").writeNullable[Date]
+    )(unlift(Car.unapply))
 
-  implicit val carReads = Json.reads[Car]
-  implicit val carWrites = Json.writes[Car]
+  implicit val carReads: Reads[Car] = (
+    (JsPath \ "id").read[Int] and
+    (JsPath \ "title").read[String] and
+    (JsPath \ "fuel").read[Fuel.Value] and
+    (JsPath \ "price").read[Int] and
+    (JsPath \ "newCar").read[Boolean] and
+    (JsPath \ "mileage").readNullable[Int] and
+    (JsPath \ "firstRegistration").readNullable[Date]
+  )(Car.apply _)
+
+//  implicit val carReads = Json.reads[Car]
+//  implicit val carWrites = Json.writes[Car]
 //  implicit val carFormat = Json.format[Car]
+  implicit val fuelReads = Reads.enumNameReads(Fuel)  //Json deserializer for type models.Fuel.Value
 
 
-  def list = Action.async { implicit request =>
+  val logger = Logger(this.getClass())
+
+
+  def list = Action.async {
     val car = carsDao.list
     car.map(cs => Ok(Json.toJson(cs)))
   }
 
-  def find(id: Int) = Action.async { implicit request =>
+  def find(id: Int) = Action.async {
     val car = carsDao.findById(id)
     car.map(cs => Ok(Json.toJson(cs)))
   }
 
   def create = Action.async(BodyParsers.parse.json) { implicit request =>
+    logger.info("Car Json ===> " + request.body)
     val carResult = request.body.validate[Car]
 
     carResult.fold(
@@ -60,13 +67,14 @@ class CarAdvertsController @Inject()(carsDao: CarsDAO)(implicit ec: ExecutionCon
         Future(BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
       },
       car => {
-        carsDao.create(car)
-        Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' saved.") )))
+        carsDao.create(car).map( res =>
+          Ok(Json.obj("status" -> "OK", "message" -> ("Car '" + car.title + "with Id " + res + "' saved.") )))
       }
     )
   }
 
   def update(id:Int) = Action.async(BodyParsers.parse.json) { implicit request =>
+    logger.info("Car Json ===> " + request.body)
     val carResult = request.body.validate[Car]
 
     carResult.fold(
@@ -74,14 +82,14 @@ class CarAdvertsController @Inject()(carsDao: CarsDAO)(implicit ec: ExecutionCon
         Future(BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
       },
       car => {
-        carsDao.update(id, car)
-        Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' updated.") )))
+        carsDao.update(id, car).map( res =>
+          Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' updated.") )))
       }
     )
   }
 
   def delete(id: Int) = Action.async { implicit request =>
-    carsDao.delete(id)
-    Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car with id '" + id + "' deleted.") )))
+    carsDao.delete(id).map( _ =>
+      Ok(Json.obj("status" ->"OK", "message" -> ("Car with id '" + id + "' deleted.") )))
   }
 }
