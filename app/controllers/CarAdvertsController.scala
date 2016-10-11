4168300522092a93b035a56acbcf9fc603f9631d
Dennis Kleine <dennis.kleine@comlearn.de>
2016-10-11 14:15:32 +0200

    update to use CarsDAO  with slick

--- app/controllers/CarAdvertsController.scala
+++ app/controllers/CarAdvertsController.scala
@@ -1,70 +1,87 @@
 package controllers
 
+import java.util.Date
 import javax.inject._
 
-import models.Car
+import models.{Car, CarsDAO, Fuel}
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class CarAdvertsController @Inject() extends Controller {
+class CarAdvertsController @Inject()(carsDao: CarsDAO)(implicit ec: ExecutionContext) extends Controller {
 
-  implicit val carWrites: Writes[Car] = (
-    (JsPath \ "id").write[Int] and
-    (JsPath \ "title").write[String]
-  )(unlift(Car.unapply))
+//  implicit val carWrites: Writes[Car] = (
+//    (JsPath \ "id").write[Int] and
+//    (JsPath \ "title").write[String] and
+//    (JsPath \ "fuel").write[Fuel.Value] and
+//    (JsPath \ "price").write[Int] and
+//    (JsPath \ "newCar").write[Boolean] and
+//    (JsPath \ "mileage").write[Option[Int]] and
+//    (JsPath \ "firstRegistration").write[Option[Date]]
+//    )(unlift(Car.unapply))
+//
+//  implicit val carReads: Reads[Car] = (
+//    (JsPath \ "id").read[Int] and
+//    (JsPath \ "title").read[String] and
+//    (JsPath \ "fuel").read[Fuel.Value] and
+//    (JsPath \ "price").read[Int] and
+//    (JsPath \ "newCar").read[Boolean] and
+//    (JsPath \ "mileage").read[Option[Int]] and
+//    (JsPath \ "firstRegistration").read[Option[Date]]
+//  )(Car.apply _)
 
-  implicit val carReads: Reads[Car] = (
-    (JsPath \ "id").read[Int] and
-    (JsPath \ "title").read[String]
-  )(Car.apply _)
+  implicit val carReads = Json.reads[Car]
+  implicit val carWrites = Json.writes[Car]
+//  implicit val carFormat = Json.format[Car]
 
 
-  def list = Action { implicit request =>
-    val json = Json.toJson(Car.list)
-    Ok(json)
+  def list = Action.async { implicit request =>
+    val car = carsDao.list
+    car.map(cs => Ok(Json.toJson(cs)))
   }
 
-  def find(id: Int) = Action { implicit request =>
-    val json = Json.toJson(Car.find(id))
-    Ok(json)
+  def find(id: Int) = Action.async { implicit request =>
+    val car = carsDao.findById(id)
+    car.map(cs => Ok(Json.toJson(cs)))
   }
 
-  def create = Action(BodyParsers.parse.json) { implicit request =>
+  def create = Action.async(BodyParsers.parse.json) { implicit request =>
     val carResult = request.body.validate[Car]
 
     carResult.fold(
       errors => {
-        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors)))
+        Future(BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
       },
       car => {
-        Car.create(car)
-        Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' saved.") ))
+        carsDao.create(car)
+        Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' saved.") )))
       }
     )
   }
 
-  def update(id:Int) = Action(BodyParsers.parse.json) { implicit request =>
+  def update(id:Int) = Action.async(BodyParsers.parse.json) { implicit request =>
     val carResult = request.body.validate[Car]
 
     carResult.fold(
       errors => {
-        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors)))
+        Future(BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
       },
       car => {
-        Car.update(id, car)
-        Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' updated.") ))
+        carsDao.update(id, car)
+        Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car '" + car.title + "' updated.") )))
       }
     )
   }
 
-  def delete(id: Int) = Action { implicit request =>
-    Car.delete(id)
-    Ok("car deleted")
+  def delete(id: Int) = Action.async { implicit request =>
+    carsDao.delete(id)
+    Future(Ok(Json.obj("status" ->"OK", "message" -> ("Car with id '" + id + "' deleted.") )))
   }
 }
