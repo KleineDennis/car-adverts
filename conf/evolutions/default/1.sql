484daeed712f2fc56a774a3971ffbf47360cd036
Dennis Kleine <dennis.kleine@comlearn.de>
2016-10-16 20:04:55 +0200

    insert some trial data

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,7 +1,9 @@
 # --- !Ups
 
 create table "CAR" ("ID" bigserial primary key not null, "TITLE" varchar not null, "FUEL" varchar not null, "PRICE" int not null, "NEWCAR" boolean not null, "MILEAGE" int, "FIRST_REGISTRATION" bigint);
-
+insert into "CAR" values (1011, 'Audi A4 Avant', 'Gasoline', 10001, false, 0, 1355266800000);
+insert into "CAR" values (1017, 'Audi', 'Gasoline', 16000, false, 0, 1355266800000);
+insert into "CAR" values (1020, 'BMW', 'Diesel', 16001, true, 100000, 1355266800000);
 # --- !Downs
 
 drop table "CAR";
